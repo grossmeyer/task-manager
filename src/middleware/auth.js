64ab980d72478433df761c1c3710889d4910b79e
Glenn Meyer <grossmeyer@gmail.com>
2021-05-20 21:50:32 -0400

    Update auth.js
    Removed unnecessary space in function parameters

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
-const auth = async ( req, res, next) => {
+const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
